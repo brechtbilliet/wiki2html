fa4f1f4ad26a904cd3f5512fd0c15c5bcb541606
elia.contini <elia.contini@02330a1f-ba08-9ff6-ec9b-a5b086fa85dd>
2011-08-01 08:36:45 +0000

    Refactoring and test on Apple Mac OS X 10.6.8, MS Windows 7, Canonical Ubuntu GNU/Linux 11.04 with the last version of  - Chrome / Chromium  - Firefox  - Safari  - IE  - Opera

--- js/wiki2html.js
+++ js/wiki2html.js
@@ -1,3 +1,21 @@
+/*
+	wiki2HTML Parses wiki markup and generates HTML 5 showing a preview.
+    Copyright (C) 2010-2011 Elia Contini
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/regexp
 
 function wiki2html(wikicode)
